31b0ffcfe4853401aeda0dd97c36e03df229a203
wendycorzo <wendyvargas@colegioguanenta.edu.co>
2022-04-21 18:40:16 -0500

    Desarrollo diagrama de flujo

---
+++ temperatura.py
@@ -0,0 +1,9 @@
+# input
+c=int (input("Digite el valor de los grados celsius: "))
+
+# procesing
+k=c+273.15
+f=1.8*c+32
+
+#output
+print(str(c)+ " grados celsius " + " en kelvin es " + str(k) + " y en farenheit " + str(f))
